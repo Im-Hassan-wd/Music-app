710ca3742605104cf506763bd9056e349464892e
Im-Hassan-wd <taiwohusayn5@gmail.com>
2021-09-07 19:46:42 +0100

    playing the songs

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
 const app = () => {
     const play = document.querySelector('.play');
-    const song = document.querySelector('.song');
+    const sound = document.querySelector('.song');
+    const outline = document.querySelector('.moving-outline rect');
+    const image = document.querySelector('.player img');
 
+    //songs
+    const songs = document.querySelectorAll('.library button');
+    //get length of outline
+    const outlineLength = outline.getTotalLength();
+    //duration
+    let duration = 600;
+
+    outline.style.strokeDasharray = outlineLength;
+    outline.style.strokeDashoffset = outlineLength;
+
+    //play song
     play.addEventListener('click', () => {
-        song.play();
-        play.src = 'svg/pause.svg';
+        checkPlaying(sound);
     });
 
-    function playMusic() {
-       
+    //stop and play song
+    const checkPlaying = sound => {
+        if(sound.paused){
+            sound.play();
+            play.src = 'svg/pause.svg';
+        } else {
+            sound.pause();
+            play.src = 'svg/play.svg';
+        }
+    };
+
+    //animate the time
+    sound.ontimeupdate = () => {
+        let currentTime = sound.currentTime;
+        let elapsed = duration - currentTime;
+        let seconds =Math.floor(elapsed % 60);
+        let minute = Math.floor(elapsed / 60);
+
+        // animate the outline
+        let progress = outlineLength - (currentTime / duration) * outlineLength;
+        outline.style.strokeDashoffset = progress;
     }
 }
 
-app()
+app();
