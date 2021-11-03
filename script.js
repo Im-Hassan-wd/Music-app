3df3e1009d6ccbb59f9a5b83170617db45c865b3
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2021-11-03 20:51:22 +0100

    it's been a while, enabling javascript :)

--- script.js
+++ script.js
@@ -2,11 +2,64 @@ const app = () => {
     const play = document.querySelector('.play');
     const sound = document.querySelector('audio');
     const slider = document.querySelector('.control-slider');
-    const spin = document.querySelector('.player img');
+    const thumbnail = document.querySelector('.player img');
     const title = document.querySelector('.title');
     const artist = document.querySelector('.sub-title');
     const mute = document.querySelector('.mute');
     const start = document.querySelector('.start');
+    const next = document.querySelector(".next");
+    const songs = document.querySelectorAll('.library button');
+
+    // next songs generate
+        //music
+    let musics = [
+        "hollow.mp3",
+        "Keep Going.mp3",
+        "Rainy-Nights.mp3",
+        "in-and-out.mp3",
+        "nothing-really-matters.mp3",
+        "pay-up.mp3",
+        "sanctuary.mp3",
+        "lazarus.mp3",
+        "disconnect.mp3"
+    ];
+        //images
+    let thumbnails = [
+        "hollow.jpeg",
+        "keep-going.jpg",
+        "rainy-night.jpeg",
+        "in-and-out.jpg",
+        "nothing.jpeg",
+        "pay-up.jpeg",
+        "sanctuary.jpeg",
+        "disconnect.jpeg",
+        "himalaya.jpeg"
+    ];
+        //titles
+    let titles = [
+        "Hollow",
+        "Keep Going",
+        "Rainy Night",
+        "In And Out Of Love",
+        "Nothing really matters",
+        "Pay up",
+        "Sanctuary",
+        "In A Cabin By The Lake",
+        "Disconnect"
+    ]
+        //artist
+    let artists = [
+        "Ecepta",
+        "Sworn",
+        "Azaleh",
+        "The Eastern Plain",
+        "Astroblk",
+        "Astroblk",
+        "Aviscerall",
+        "Lazarus Moment",
+        "Victoriya"
+    ]
+
     // get length of slider
     let sliderLength = slider.max;
     slider.value = sliderLength;
@@ -15,27 +68,29 @@ const app = () => {
     // library
     const library = document.querySelector('.library');
     const button = document.querySelector('.header button');
-    const music = document.querySelector('.music');
+    const musicPage = document.querySelector('.music');
 
     // event listerners
     button.addEventListener('click', showLibrary);
-    music.addEventListener('click', (e) => closeLibrary(e));
+    musicPage.addEventListener('click', (e) => closeLibrary(e));
     // slider.addEventListener("change", (e) => fastForward(e));
     //play song
     play.addEventListener('click', () => checkPlaying(sound));
     // mute song
     mute.addEventListener("click", () => muteSong(sound));
+    //next song 
+    next.addEventListener("click", () => playNext());
 
     //functions 
     function closeLibrary(e) {
         if (e.target.tagName != 'BUTTON' && e.target.tagName !='IMG' && e.target.tagName !='svg') {
             library.classList.remove('show-library');
-            music.classList.remove('translate');
+            musicPage.classList.remove('translate');
         }
     }
     function showLibrary(){
         library.classList.toggle('show-library');
-        music.classList.toggle('translate');
+        musicPage.classList.toggle('translate');
     }
 
     function fastForward(e){
@@ -46,17 +101,17 @@ const app = () => {
 
 
     //songs
-    const songs = document.querySelectorAll('.library button');
-    songs.forEach(song => {
+    songs.forEach((song,e) => {
         song.addEventListener('click', function() {
             sound.src = this.getAttribute('data-song');
-            spin.src = this.getAttribute('data-img');
-            title.innerText = this.getAttribute('data-title');
+            thumbnail.src = this.getAttribute('data-img');
+            title.textContent = this.getAttribute('data-title');
             artist.textContent = this.getAttribute('data-artist');
             checkPlaying(sound);
             sound.muted = false;
         });
     });
+
 
     //mute and un mute song 
     const muteSong = sound => {
@@ -81,6 +136,20 @@ const app = () => {
         }
     };
 
+    const playNext = () => {
+        // index
+        let index = Math.floor(Math.random() * musics.length);
+        const nextSong = musics[index];
+
+        //music, thumbnail, title, artist
+        sound.src = "music/" + nextSong;
+        thumbnail.src = "img/" + thumbnails[index];
+        title.textContent = titles[index];
+        artist.textContent = artists[index];
+        sound.play();
+        play.querySelector('img').src = 'svg/pause.svg';
+    }
+
 
     //animate the time and update the lenght and interver of each songs
     sound.ontimeupdate = (e) => {
@@ -96,8 +165,8 @@ const app = () => {
 
         //outputing the current time
         start.textContent = `${minutes}:${seconds}`;
-        spin.style.transform = 'rotate(' + currentTime * 10 + 'deg)';
-        spin.style.transition = 'all 0.2s linear';
+        thumbnail.style.transform = 'rotate(' + currentTime * 10 + 'deg)';
+        thumbnail.style.transition = 'all 0.2s linear';
 
 
         // duration
